5a2fcc3a40efe5e3efb9f1d359ec49107cdca930
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-09-13 10:33:23 -0300

    configs

--- platformgame/scripts/player.gd
+++ platformgame/scripts/player.gd
@@ -1,25 +1,47 @@
 extends CharacterBody2D
 
+class_name player
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+enum playernode {
+	SMALL,
+	BIG,
+	SHOOTING
+}
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+# referencias
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var area_collision_shape = $Area2D/AreaCollisionShape
+@onready var body_collision_shape = $BodyCollisionShape
+
+@export_group("Locomotion")
+@export var run_speed_damping = 0.5
+@export var speed = 300.0
+@export var jump_velocity = -350
+@export_group("")
+
+
+func _physics_process(delta):
+	
+	#aplciando grvaidade
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+		velocity.y += gravity * delta
+		
+	#pulos
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.5 
+	
+#	Movimentacao
+	var direction = Input.get_axis("left", "right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = lerpf(velocity.x, speed * direction, run_speed_damping * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+	
 	move_and_slide()
---
+++ platformgame/scripts/player_animated_sprite.gd
@@ -0,0 +1,6 @@
+extends AnimatedSprite2D
+
+class_name PlayerAnimatedSprite
+
+#func trigger_animation(velocity: Vector2, direction: int, player_mode: player.PlayerMode):
+	#pass
